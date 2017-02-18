fbce1821e396d8c11f4cda47e5ee6dd543a84582
Sarath LUN <lun.sarath@gmail.com>
2017-02-18 16:33:39 +0700

    System log @ Verify login
    - when verify login the system log can’t log user_id so it need to be
    null.

--- application/controllers/authentication/authentication_c.php
+++ application/controllers/authentication/authentication_c.php
@@ -22,20 +22,21 @@ class Authentication_c extends CI_Controller
 	
 	public function validate_login()
 	{
-		$this->form_validation->set_rules('email_login','','required|valid_email');
-		$this->form_validation->set_rules('password_login','','required|min_length[1]');
+		$this->form_validation->set_rules('email_login', '', 'required|valid_email');
+		$this->form_validation->set_rules('password_login', '', 'required|min_length[1]');
 		/*todo->sarath: in production set password_login => min_length[8]*/
-		if ($this->form_validation->run()==true){
+		if ($this->form_validation->run() == true) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
 	}
 	
 	public function verify_login()
 	{
+		$log_user = null;
 		$log_action = 'verify_login';
-		if ($this->validate_login()==true) {
+		if ($this->validate_login() == true) {
 			//todo->sarath: need encryption on password
 			$email = $this->input->post('email_login');
 			$password = $this->input->post('password_login');
@@ -49,8 +50,8 @@ class Authentication_c extends CI_Controller
 				$user = $this->authentication_m->check_password_correct($cri);
 				if ($user != false) {
 					//login correct
-					$log_msg = 'log in successful with '.$email;
-					$this->my_library->do_system_logs($email,$log_action,$log_msg);
+					$log_msg = 'log in successful with ' . $email;
+					$this->my_library->do_system_logs($log_user, $log_action, $log_msg);
 					// set session with encrypted data
 					$user_data = array(
 						'uid' => $this->encryption->encrypt($user['user_id'])
@@ -60,12 +61,12 @@ class Authentication_c extends CI_Controller
 					//route to each sub system by group
 					$this->route_to_sub_system($user['user_gid']);
 				} else {
-					$log_msg = 'incorrect password with user '.$email;
+					$log_msg = 'incorrect password with user ' . $email;
 					//incorrect password
 					$msg_body = 'Your password is not correct.<br>Please verify password and try again.';
 				}
 			} else {
-				$log_msg = 'user '.$email.' is not existed.';
+				$log_msg = 'user ' . $email . ' is not existed.';
 				//user not exist
 				$msg_body = 'Your email address doesn\'t exist for login.<br>Please verify your email address and try again.';
 			}
@@ -74,8 +75,8 @@ class Authentication_c extends CI_Controller
 			//validate fail
 			$msg_body = 'Both email and password are required for login!';
 		}
-		$this->my_library->do_system_logs('N/A',$log_action,$log_msg);
-		$msg = $this->my_library->generate_alert('danger','Login Fail !',$msg_body);
+		$this->my_library->do_system_logs($log_user, $log_action, $log_msg);
+		$msg = $this->my_library->generate_alert('danger', 'Login Fail !', $msg_body);
 		$this->session->set_flashdata('msg', $msg);
 		redirect('authentication/authentication_c/login_form');
 	}
@@ -89,7 +90,7 @@ class Authentication_c extends CI_Controller
 			//todo->sarath: more cases come here to complete all system levels!
 			default:
 				$msg_body = 'You Don\'t Have Permission To Login.';
-				$msg = $this->my_library->generate_alert('danger','Login Fail !',$msg_body);
+				$msg = $this->my_library->generate_alert('danger', 'Login Fail !', $msg_body);
 				$this->session->set_flashdata('msg', $msg);
 				redirect('authentication/authentication_c/login_form');
 				break;
@@ -102,13 +103,13 @@ class Authentication_c extends CI_Controller
 		$uid = $this->encryption->decrypt($this->session->userdata('uid'));
 		$u_info = $this->my_library->get_user_info($uid);
 		$log_user = $u_info['user_id'];
-		$log_action= 'logout';
-		$log_msg = 'logged out by '.$u_info['user_email'];
+		$log_action = 'logout';
+		$log_msg = 'logged out by ' . $u_info['user_email'];
 		$this->my_library->do_system_logs($log_user, $log_action, $log_msg);
 		//unset userdata
 		$data = array('uid');
 		$this->session->unset_userdata($data);
 		$this->session->sess_destroy();
-		redirect('authentication/authentication_c/login_form','refresh');
+		redirect('authentication/authentication_c/login_form', 'refresh');
 	}
 }
